ac9b43765d5c041a1416ef927684c1a17f3ec3cf
Alexia Tobares <avtobares@gmail.com>
2023-07-16 17:11:00 -0300

    cambios preentrega 2

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 /*este va a ser un programa destinado a ingresar calificaciones de alumnos y calcular sus promedios anuales*/
 
 const Prom = (n1,n2,n3,n4) => (n1 + n2 + n3 + n4) / 4;
+
+function Alumnno(nombre, n1, n2, n3, n4, prom, cond) {
+    this.nombre = nombre;
+    this.nota1 = n1;
+    this.nota2 = n2;
+    this.nota3 = n3;
+    this.nota4 = n4;
+    this.promedio = prom;
+    this.condicion = cond;
+};
+
 let crearnombre;
 let crearclave;
 let nombre;
@@ -14,6 +25,10 @@ let calif4;
 let promedio; 
 let estado;
 let respuesta = "si"
+let alumno
+let curso = []
+let promedios = []
+let nombreant = " "
 alert("Bienvenido profesor/a!")
 
 while (respuesta == "si" || respuesta == "no") {
@@ -28,6 +43,10 @@ while (respuesta == "si" || respuesta == "no") {
             nombre = prompt("Ingrese su nombre completo:")
             clave = prompt("Ingrese su clave:")
             if (crearnombre == nombre && crearclave == clave) {
+                if (nombreant != nombre) {
+                        curso = [];
+                        promedios = [];
+                }
                 cantalumnos = parseInt(prompt("Ingrese la cantidad de alumnos:"))
                 for (let i = 0; i < cantalumnos; i++) {
                     nombreal = prompt("Ingrese nombre completo del alumno:");
@@ -35,15 +54,22 @@ while (respuesta == "si" || respuesta == "no") {
                     calif2 = parseFloat(prompt("Ingrese la calificacion del examen 2:"))
                     calif3 = parseFloat(prompt("Ingrese la calificacion del examen 3:"))
                     calif4 = parseFloat(prompt("Ingrese la calificacion del examen 4:"))
+                    
                     promedio = Prom(calif1, calif2, calif3, calif4)
                     if (promedio >= 6) {
-                        estado = "Aprobado"
+                        estado = "Aprobado/a"
                     } else {
-                        estado = "Desaprobado"
+                        estado = "Desaprobado/a"
                     }
-                    console.log(i + " " + nombreal + ":" + "Promedio:" + promedio + " " + "Condición:" + estado)
-
+                    alumno = new Alumnno(nombreal,calif1, calif2, calif3, calif4, promedio, estado)
+                    console.log(alumno)
+                    
+                    curso.push(alumno.nombre)
+                    promedios.push(alumno.promedio)
                 }
+                console.log("Estudiantes profesor/a" + " " + nombre + ":" + curso)
+                console.log("Promedios en orden descendente de este curso:" + promedios.sort((a,b)=> b - a))
+                nombreant = nombre
                 respuesta = prompt("¿Desea seguir utilizando la plataforma?(finalizar/si)")
             } else {
                 alert("Usuario o contraseña incorrectos!")
